4d95f14e399641fa952235f1971bbab08134af48
Derek Organ <derek@1timetracking.com>
2011-09-22 11:23:39 -0400

    Installed login plugin (not yet working fully)

---
+++ lib/form/doctrine/base/BaseProfileForm.class.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Profile form base class.
+ *
+ * @method Profile getObject() Returns the current form's model object
+ *
+ * @package    registration
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormGeneratedTemplate.php 29553 2010-05-20 14:33:00Z Kris.Wallsmith $
+ */
+abstract class BaseProfileForm extends BaseFormDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'id'               => new sfWidgetFormInputHidden(),
+      'sf_guard_user_id' => new sfWidgetFormDoctrineChoice(array('model' => $this->getRelatedModelName('User'), 'add_empty' => true)),
+      'first_name'       => new sfWidgetFormInputText(),
+      'middle_name'      => new sfWidgetFormInputText(),
+      'last_name'        => new sfWidgetFormInputText(),
+      'email_address'    => new sfWidgetFormInputText(),
+    ));
+
+    $this->setValidators(array(
+      'id'               => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
+      'sf_guard_user_id' => new sfValidatorDoctrineChoice(array('model' => $this->getRelatedModelName('User'), 'required' => false)),
+      'first_name'       => new sfValidatorString(array('max_length' => 255, 'required' => false)),
+      'middle_name'      => new sfValidatorString(array('max_length' => 255, 'required' => false)),
+      'last_name'        => new sfValidatorString(array('max_length' => 255, 'required' => false)),
+      'email_address'    => new sfValidatorString(array('max_length' => 255, 'required' => false)),
+    ));
+
+    $this->widgetSchema->setNameFormat('profile[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'Profile';
+  }
+
+}
