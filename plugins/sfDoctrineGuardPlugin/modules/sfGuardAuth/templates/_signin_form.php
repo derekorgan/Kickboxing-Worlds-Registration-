04b8bfbc0d2836f63ffa79749c658c74f0c2c5c2
dnizetic <denis_nizetic@hotmail.com>
2011-09-27 23:09:16 +0300

    Edited plugins/sfDoctrineGuardPlugin/modules/sfGuardAuth/templates/_signin_form.php via GitHub

--- plugins/sfDoctrineGuardPlugin/modules/sfGuardAuth/templates/_signin_form.php
+++ plugins/sfDoctrineGuardPlugin/modules/sfGuardAuth/templates/_signin_form.php
@@ -1,6 +1,27 @@
 <?php use_helper('I18N') ?>
 
-<form action="<?php echo url_for('@sf_guard_signin') ?>" method="post">
+<form class="form label-inline uniform" action="<?php echo url_for('@sf_guard_signin') ?>" method="post">
+    
+    <?php  ?>
+    <div class="field">
+        
+        <?php echo $form['username']->renderLabel('Username') ?>
+        <?php echo $form['username']->render() ?>
+        <?php echo $form['username']->renderError() ?>
+    </div>
+    
+    <div class="field">
+        <?php echo $form['password']->renderLabel('Password') ?>
+        <?php echo $form['password']->render() ?>
+        <?php echo $form['password']->renderError() ?>
+    </div>
+    
+    
+  <div><?php echo $form['_csrf_token']->render() ?></div>
+  
+  <div class="buttonrow"><input class="btn btn-black btn-login" type="submit" value="<?php echo __('Login', null, 'sf_guard') ?>" /></div>
+    
+    <?php /* ?>
   <table>
     <tbody>
       <?php echo $form ?>
@@ -22,4 +43,7 @@
       </tr>
     </tfoot>
   </table>
+   */  ?>
+  
+  
 </form>
