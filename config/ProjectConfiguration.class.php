35709905190682b5b0d8c39db287d2e252b11113
Derek Organ <derek@1timetracking.com>
2011-09-27 06:23:13 -0400

    Trying to install sfGuardDoctrine plugin but getting error when building tables

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -7,8 +7,9 @@ class ProjectConfiguration extends sfProjectConfiguration
 {
   public function setup()
   {
-    $this->enablePlugins('sfDoctrinePlugin');
-
-    $this->enablePlugins('sfDoctrineGuardPlugin');
+	$this->enablePlugins(array(
+		'sfDoctrinePlugin',
+		'sfDoctrineGuardPlugin'
+	));
   }
 }
