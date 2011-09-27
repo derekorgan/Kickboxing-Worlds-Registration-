c401436c11024400ade92f07bda329d62ff2aa35
dnizetic <denis_nizetic@hotmail.com>
2011-09-27 23:04:52 +0300

    Edited plugins/sfDoctrineGuardPlugin/modules/sfGuardRegister/templates/indexSuccess.php via GitHub

--- plugins/sfDoctrineGuardPlugin/modules/sfGuardRegister/templates/indexSuccess.php
+++ plugins/sfDoctrineGuardPlugin/modules/sfGuardRegister/templates/indexSuccess.php
@@ -1,4 +1,32 @@
 <?php use_helper('I18N') ?>
-<h1><?php echo __('Register', null, 'sf_guard') ?></h1>
 
-<?php echo get_partial('sfGuardRegister/form', array('form' => $form)) ?>
+
+
+
+
+
+            
+  	<h2 class="header-padding">Create Account</h2>
+		<div class="login-left">
+                    
+                    <?php echo get_partial('sfGuardRegister/form', array('form' => $form)) ?>
+			
+			
+		</div>
+		
+		<div class="login-right">
+			<h4>Please fill in some information about yourself an we will send you an email to confirm your login details.</h4>
+			<h4>When your login details are confirmed you can access your account at anytime to register attendees.</h4>
+		</div>
+                
+
+
+
+<h1><?php //echo __('Register', null, 'sf_guard') ?></h1>
+
+<?php //echo get_partial('sfGuardRegister/form', array('form' => $form)) ?>
+
+
+<?php 
+
+?>
