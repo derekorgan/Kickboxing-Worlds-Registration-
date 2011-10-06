c20d0e85ffe0400de4be07da97cb8f971a48e82f
Derek Organ <derek@1timetracking.com>
2011-10-06 03:48:18 +0100

    configured the index file so it sets enviroments depending on the url request

--- web/index.php
+++ web/index.php
@@ -3,5 +3,20 @@
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
-sfContext::createInstance($configuration)->dispatch();
+$theurl = explode('/', $_SERVER['PHP_SELF']);
+$mydir = $theurl[1];
+
+
+if ($mydir == "kickboxing")  //live
+{    
+    $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'prod', false);
+    sfContext::createInstance($configuration)->dispatch();
+}
+else // dev 
+{    
+    $configuration = ProjectConfiguration::getApplicationConfiguration('frontend', 'dev', true);
+    sfContext::createInstance($configuration)->dispatch();
+}
+
+
+
